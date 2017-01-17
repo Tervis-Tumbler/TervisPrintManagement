93dea985dac4f0900ff2cb4856cf2fd4bd0ba66d
hambone124 <hperez@tervis.com>
2017-01-17 15:09:43 -0500

    Added Operation Validation for this module
    Run the following:
    Get-OperationValidation -ModuleName TervisPrintManagement |
    Invoke-OperationValidation -IncludePesterOutput

---
+++ Diagnostics/Simple/Simple.Tests.ps1
@@ -0,0 +1,16 @@
+﻿Describe "Printer Driver Validation" {
+    #$PrintServer = "disney"
+    #$Session = New-PSSession -ComputerName $PrintServer
+    $PrinterDriverRegKeyPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Print\Environments\Windows x64\Drivers\Version-3"
+
+    $PrinterDrivers = Get-ChildItem $PrinterDriverRegKeyPath | select -ExpandProperty PSChildName    
+
+    foreach ($PrinterDriver in $PrinterDrivers) {
+        $FullPathToPrinterDriver = Join-Path -Path $PrinterDriverRegKeyPath -ChildPath $PrinterDriver
+        $PrinterDriverProperties = Get-ItemProperty -Path $FullPathToPrinterDriver -Name PrinterDriverAttributes
+        $PackagedStatus = $PrinterDriverProperties.PrinterDriverAttributes -band 1      
+        It "$PrinterDriver should be set to Packaged" {
+            $PackagedStatus | Should be 1
+        }
+    }
+}
