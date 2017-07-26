7d4f1f5df6abda24e9effa1c2bbb4edf4b0d1d48
ChrisMagnuson <hourglasssand@gmail.com>
2017-07-26 13:49:18 -0400

    Indent

--- TervisPrintManagement.psm1
+++ TervisPrintManagement.psm1
@@ -41,16 +41,16 @@ Function Set-TervisPrinterMetadataMember {
 }
 
 Function Get-TervisPrinterLifeTimePageCount {
-$SNMP = new-object -ComObject olePrn.OleSNMP
+    $SNMP = new-object -ComObject olePrn.OleSNMP
 
-$snmp.open("10.172.28.13","public",2,3000)
-$snmp.gettree("43.11.1.1.8.1")
-$printertype = $snmp.Get(".1.3.6.1.2.1.25.3.2.1.3.1")
+    $snmp.open("10.172.28.13","public",2,3000)
+    $snmp.gettree("43.11.1.1.8.1")
+    $printertype = $snmp.Get(".1.3.6.1.2.1.25.3.2.1.3.1")
 
-$snmp.open("10.172.24.116","public",2,3000)
-$snmp.gettree("43.11.1.1.8.1")
-$printertype = $snmp.Get(".1.3.6.1.2.1.25.3.2.1.3.1")
-$snmp.get(".1.3.6.1.2.1.43.10.2.1.4.1.1")  
+    $snmp.open("10.172.24.116","public",2,3000)
+    $snmp.gettree("43.11.1.1.8.1")
+    $printertype = $snmp.Get(".1.3.6.1.2.1.25.3.2.1.3.1")
+    $snmp.get(".1.3.6.1.2.1.43.10.2.1.4.1.1")  
 }
 
 function Update-PrinterLocation {
